f54f4b6306d4724490a4f13e8548ee925de95a72
David Goodman & John Olmsted <pair+dgoodman1224.qsymmachus@devbootcamp.com>
2014-01-15 11:56:42 -0800

    Adds working versions of the Post and Comment classes

--- hn_post_scraper.rb
+++ hn_post_scraper.rb
@@ -3,7 +3,7 @@ require 'nokogiri'
 $doc = Nokogiri::HTML(open('post.html'))
 
 class Post
-  attr_reader :title, :url, :points, :item_id
+  attr_reader :title, :url, :points, :item_id, :comments
 
   def initialize(post_doc)
     @title =  post_doc.search('.title > a:first-child').map { |link| link.inner_text}.pop
@@ -11,8 +11,20 @@ class Post
     @points = post_doc.search('.subtext > span:first-child').map { |span| span.inner_text}.pop
     @item_id = post_doc.search('.subtext > a:nth-child(3)').map { |link| link['href']}.pop
     @item_id = @item_id.match(/\d+/).to_s.to_i
-    # @comments = [Comments.new]
+    @all_comments = $doc.search('.comment')
+    @all_comheads = $doc.search('.comhead')
+    @comments = Array.new
+    create_comments
   end
+
+  def create_comments
+    counter = 0
+    until counter >= @all_comments.length
+      @comments << Comment.new(@all_comments[counter], @all_comheads[counter + 1])
+      counter += 1
+    end
+  end
+
 end
 
 
@@ -25,10 +37,7 @@ class Comment
     @item_id = nokogiri_comhead.search('a:nth-child(2)')[0].attributes['href'].value
     @item_id = @item_id.match(/\d+/).to_s.to_i
   end
-
 end
-
-
 
 
 
@@ -38,9 +47,11 @@ our_post = Post.new($doc)
 # p our_post.url
 # p our_post.points
 # p our_post.item_id
-test_nokogiri_comhead = $doc.search('.comhead')[1]
-test_nokogiri_comment = $doc.search('.comment')[0]
-test_comment = Comment.new(test_nokogiri_comment, test_nokogiri_comhead)
-p test_comment.user
-p test_comment.text
-p test_comment.item_id
+# test_nokogiri_comhead = $doc.search('.comhead')[1]
+# test_nokogiri_comment = $doc.search('.comment')[0]
+# test_comment = Comment.new(test_nokogiri_comment, test_nokogiri_comhead)
+# p test_comment.user
+# p test_comment.text
+# p test_comment.item_id
+# p our_post.all_comments[0]
+p our_post.comments[0]
